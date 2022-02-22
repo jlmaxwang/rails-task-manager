535661e28d0c2b27824aafb3176ab45cbf1878ed
Max Jilu Wang <wangjilu23@gmail.com>
2022-02-22 16:21:37 +1100

    CRUD for model

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,54 @@
 class TasksController < ApplicationController
-  def create
-    raise
+  before_action :set_task, only: [:show, :edit, :update, :destroy]
+
+  def index
+    # list all task
+    @tasks = Task.all
   end
 
+  def show
+  end
+
+  def new
+    @task = Task.new
+  end
+
+  def create
+    @task = Task.new(task_params)
+    if @task.save
+      redirect_to task_path(@task.id)
+    else
+      render :new
+    end
+  end
+
+  def save
+    @task.save
+  end
+
+  def edit
+  end
+
+  def update
+    if @task.update(task_params)
+      redirect_to task_path(@task.id)
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @task.destroy
+    redirect_to tasks_path
+  end
+
+  private
+
+  def set_task
+    @task = Task.find(params[:id])
+  end
+
+  def task_params
+    params.require(:task).permit(:title, :details, :completed)
+  end
 end
