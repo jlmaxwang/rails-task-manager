1a8541793bf5a708ca196a043b64cdf68fd1408f
Max Jilu Wang <wangjilu23@gmail.com>
2022-02-22 16:22:42 +1100

    routes created

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,12 @@
 Rails.application.routes.draw do
-  get 'create', to: 'tasks#create'
+  # get 'tasks', to: 'tasks#index', as: :tasks
+  # get 'tasks/new', to: 'tasks#new', as: :new_task
+  # get 'tasks/:id', to: 'tasks#show', as: :task
+  # post 'tasks', to: 'tasks#create'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit_task
+  # patch 'tasks/:id', to: 'tasks#update'
+  # delete 'tasks/:id', to: 'tasks#destory'
+  resources :tasks
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
